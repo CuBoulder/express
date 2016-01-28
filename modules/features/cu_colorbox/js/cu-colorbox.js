25382651f8568edf06c8245cc614f3b26e826b28
kevincrafts <kevin.crafts@colorado.edu>
2016-01-27 21:45:55 -0700

    fixing issue where colorbox images placed in wysiwyg field would not let you step through images once opened in colorbox FIT-1222

--- modules/features/cu_colorbox/js/cu-colorbox.js
+++ modules/features/cu_colorbox/js/cu-colorbox.js
@@ -3,6 +3,7 @@
     $("a.colorbox").each(function(){
       var imgAlt = $("img", this).attr("alt");
       $(this).attr("title", imgAlt);
+      $(this).attr("rel", "colorbox-gallery");
     });
   });
 })( jQuery );
