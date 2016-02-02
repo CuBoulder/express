f74e5157fd60e6d6c134defbef8c078e94ad2788
kevincrafts <kevin.crafts@colorado.edu>
2016-02-02 12:27:50 -0700

    increasing line height, removing vertical margin on link FIT-1409

--- modules/custom/cu_alerts/cu_alerts.tpl.php
+++ modules/custom/cu_alerts/cu_alerts.tpl.php
@@ -12,7 +12,7 @@
     <div class="alert">
       <?php print $alert->title; ?>
       <?php if (!empty($alert->alert_url)) : ?>
-        <?php print l('Read&nbsp;More&nbsp;&raquo;', $alert->alert_url, array('html' => TRUE)); ?>
+        <?php print l('Read More &raquo;', $alert->alert_url, array('html' => TRUE)); ?>
       <?php endif; ?>
     </div>
   <?php endforeach; ?>
