bace66b7e44614b16dc3209a9dd29442821768b4
kevincrafts <kevin.crafts@colorado.edu>
2016-03-18 11:26:34 -0600

    adding class, markup for medium.com style title image FIT-1687

--- modules/custom/express_layout/templates/express-layout-title-image.tpl.php
+++ modules/custom/express_layout/templates/express-layout-title-image.tpl.php
@@ -1,8 +1,12 @@
+<div class="title-image-breadcrumbs-wrapper">
+  <div class="title-image-breadcrumbs element-max-width-padding"></div>
+</div>
 <div class="feature-layout-title-image" style="background-image:url(<?php print $img; ?>)">
   <div class="feature-layout-title-image-overlay">
     <div class="feature-layout-title-image-page-title element-max-width-padding">
       <h1><?php print $title; ?></h1>
-      <?php //print theme('breadcrumb', array('breadcrumb'=>drupal_get_breadcrumb())); ?>
+      <div class="title-image-byline"></div>
+
     </div>
   </div>
 </div>
