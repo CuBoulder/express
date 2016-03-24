f66cf820da6e2676636c7c1cce2131ad9e1760b7
kevincrafts <kevin.crafts@colorado.edu>
2016-03-24 10:32:26 -0600

    moving breadcrumb markup FIT-1687

--- modules/custom/express_layout/templates/express-layout-title-image.tpl.php
+++ modules/custom/express_layout/templates/express-layout-title-image.tpl.php
@@ -1,7 +1,8 @@
-<div class="title-image-breadcrumbs-wrapper">
-  <div class="title-image-breadcrumbs element-max-width-padding"></div>
-</div>
+
 <div class="feature-layout-title-image">
+  <div class="title-image-breadcrumbs-wrapper">
+    <div class="title-image-breadcrumbs element-max-width-padding"></div>
+  </div>
   <div class="feature-layout-title-image-overlay">
     <div class="feature-layout-title-image-page-title element-max-width-padding">
       <h1><?php print $title; ?></h1>
