0bbd0a41b1323a37492aff8a452d86913e56bdd6
kevincrafts <kevin.crafts@colorado.edu>
2016-04-07 12:44:07 -0600

    removing grid classes from widget tpl FIT-1753

--- modules/custom/express_dashboards/templates/express-dashboard-widget.tpl.php
+++ modules/custom/express_dashboards/templates/express-dashboard-widget.tpl.php
@@ -1,4 +1,4 @@
-<div class="express-dashboard-widget-container col-lg-4 col-md-6 col-sm-12 col-xs-12">
+<div class="express-dashboard-widget-container">
     <h2 class="express-dashboard-widget-title"><?php print $title; ?></h2>
     <div class="express-dashboard-widget-content">
       <?php print render($content); ?>
