57bb4a45cd3be7d7bc1fafafc3b30be9f1844326
Alex Finnarn <alexfinnarn@gmail.com>
2016-05-26 15:16:27 -0600

    making quicktabs name a variable

--- modules/custom/quicktab_beans/js/qt_pager.js
+++ modules/custom/quicktab_beans/js/qt_pager.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.quicktabBeansPager = {
     attach: function (context, settings) {
-      var tabs, doop, href;
+      var tabs, doop, href, qtName;
 
       // forEach method, could be shipped as part of an Object Literal/Module
       var forEach = function (array, callback, scope) {
@@ -10,8 +10,10 @@
         }
       };
 
-      // Get all quicktabs tabs
+      // Get all quicktabs tabs.
       tabs = document.querySelectorAll('.quicktabs-tabpage');
+      // Get name of quicktabs instance.
+      qtName = document.querySelector('.quicktabs-wrapper').getAttribute('id').split('-');
 
       // Loop through and add the tab reference to the pager link.
       forEach(tabs, function (index, value) {
@@ -21,7 +23,7 @@
           // Don't add query parameter if it has been added already.
           if (href.indexOf('&qt-news=') === -1) {
             // Add new href here.
-            href = href + '&qt-news=' + index;
+            href = href + '&qt-' + qtName[1] + '=' + index;
             valued.setAttribute('href', href);
           }
         });
