26dc6593f4d4bd580b67c1486534f6ac5842ddd8
kevincrafts <kevin.crafts@colorado.edu>
2016-09-16 17:12:00 -0600

    fixing issue where rss feed title was not properly FIT-2141

--- modules/custom/cu_rss/templates/views-view-rss--article-rss-feed.tpl.php
+++ modules/custom/cu_rss/templates/views-view-rss--article-rss-feed.tpl.php
@@ -10,13 +10,16 @@
  // Replace feed title with saved feed entity name
  // Only if a feed entity at this url exists
  global $base_url;
- $current_url = $base_url . '/' . current_path();
+ $path = current_path();
+ if ($path == 'feed/all/all/rss.xml') {
+   $path = 'feed/rss.xml';
+ }
+ $current_url = $base_url . '/' . $path;
  $query = db_select('rss_feed', 'rf');
  $query->fields('rf', array('id', 'name', 'url'));
  $query->condition('rf.url', $current_url, '=');
  $feeds = $query->execute()->fetchAssoc();
  $feed_title = isset($feeds['name']) ? $feeds['name'] : variable_get('site_name');
-
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
 <rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
