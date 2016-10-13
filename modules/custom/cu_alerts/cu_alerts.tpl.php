cd8d6ddd0de3d9f77f052219f094d001493e2a61
kevincrafts <kevin.crafts@colorado.edu>
2016-10-13 15:19:51 -0600

    new alerts code FIT-2198

--- modules/custom/cu_alerts/cu_alerts.tpl.php
+++ modules/custom/cu_alerts/cu_alerts.tpl.php
@@ -5,16 +5,17 @@
  * CU Alerts template.
  */
 
+ $message_off = variable_get('rave_alerts_message_off', '');
 ?>
 <div id="cu-alerts" data-remote-url="<?php print $remote_url ?>">
-<?php if (!empty($data)) : ?>
-  <?php foreach ($data as $alert) : ?>
+<?php if (!empty($data['channel']['item'])) : ?>
+    <?php if ($data['channel']['item']['title'] != $message_off): ?>
     <div class="alert">
-      <?php print $alert->title; ?>
-      <?php if (!empty($alert->alert_url)) : ?>
-        <?php print l('Read More &raquo;', $alert->alert_url, array('html' => TRUE)); ?>
+      <?php print $data['channel']['item']['title']; ?>
+      <?php if (!empty($data['channel']['item']['link'])) : ?>
+        <?php print l('Read More &raquo;', $data['channel']['item']['link'], array('html' => TRUE)); ?>
       <?php endif; ?>
     </div>
-  <?php endforeach; ?>
+  <?php endif; ?>
 <?php endif; ?>
 </div>
