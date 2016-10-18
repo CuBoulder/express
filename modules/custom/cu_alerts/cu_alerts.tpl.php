f499c1f2ba75af897af261dbf5420fcf75d1bc15
Kevin Reynen <kreynen@gmail.com>
2016-10-18 13:39:55 -0600

    added all clear and active keyword triggers

--- modules/custom/cu_alerts/cu_alerts.tpl.php
+++ modules/custom/cu_alerts/cu_alerts.tpl.php
@@ -4,14 +4,10 @@
  * @file
  * CU Alerts template.
  */
-
- $message_off = variable_get('rave_alerts_message_off', '');
- $show_alerts = FALSE;
 ?>
 <div id="cu-alerts" data-remote-url="<?php print $remote_url ?>">
 <?php if (!empty($data['channel']['item'])) : ?>
-  <?php if ($data['channel']['item']['title'] != $message_off): ?>
-    <?php $show_alerts = TRUE; ?>
+  <?php if (variable_get('rave_alerts_enable', 1) && variable_get('rave_alerts_display', 1)): ?>
     <div class="alert">
       <?php print $data['channel']['item']['title']; ?>
       <?php if (!empty($data['channel']['item']['link'])) : ?>
--- modules/custom/cu_alerts/js/cu_alerts.js
+++ modules/custom/cu_alerts/js/cu_alerts.js
@@ -2,22 +2,23 @@
 
   $(document).ready(
     function alertWorker() {
-      // Create our own timestamp
-			var stamp = Math.ceil( $.now() / 10000 );
-			//console.log(stamp);
+      if ( Drupal.settings.rave_alerts_active_event == 1 ) {
+				// Create our own rounded timestamp to limit paths cached by varnish
+				var stamp = Math.ceil( $.now() / 10000 );
+				//console.log(stamp);
 
-			$.ajax({
-			  type: "GET",
-				// @TODO: get url from jQuery.extend(Drupal.settings
-				url: Drupal.settings.rave_alerts_url + "?stamp=" + stamp,
-				dataType: "xml",
-				//cache: false, // Causes Varnish to miss every request
-				success: rssParser,
-				complete: function() {
-				  // Schedule the next request when the current one's complete
-					setTimeout(alertWorker, 10000);
-				}
-			});
+				$.ajax({
+					type: "GET",
+					url: Drupal.settings.rave_alerts_url + "?stamp=" + stamp,
+					dataType: "xml",
+					//cache: false, // Causes Varnish to miss every request
+					success: rssParser,
+					complete: function() {
+						// Schedule the next request when the current one's complete
+						setTimeout(alertWorker, 10000);
+					}
+				});
+			};
     });
 
     function rssParser(xml) {
