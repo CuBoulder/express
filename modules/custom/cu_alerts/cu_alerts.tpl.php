0dc21ef2bcdd597190aa2f15ee6d8d8aad9fbd63
kevincrafts <kevin.crafts@colorado.edu>
2016-10-18 10:32:15 -0600

    adding alerts site to confits, adding alert variables to Drupal.settings, adding data attributes for debugging, fixing link markup FIT-2198

--- modules/custom/cu_alerts/cu_alerts.tpl.php
+++ modules/custom/cu_alerts/cu_alerts.tpl.php
@@ -6,14 +6,18 @@
  */
 
  $message_off = variable_get('rave_alerts_message_off', '');
+ $show_alerts = FALSE;
 ?>
 <div id="cu-alerts" data-remote-url="<?php print $remote_url ?>">
 <?php if (!empty($data['channel']['item'])) : ?>
-    <?php if ($data['channel']['item']['title'] != $message_off): ?>
+  <?php if ($data['channel']['item']['title'] != $message_off): ?>
+    <?php $show_alerts = TRUE; ?>
     <div class="alert">
       <?php print $data['channel']['item']['title']; ?>
       <?php if (!empty($data['channel']['item']['link'])) : ?>
         <?php print l('Read More &raquo;', $data['channel']['item']['link'], array('html' => TRUE)); ?>
+      <?php elseif ($rave_alerts_site = variable_get('rave_alerts_site', NULL)): ?>
+        <?php print l('Read More &raquo;', $rave_alerts_site, array('html' => TRUE)); ?>
       <?php endif; ?>
     </div>
   <?php endif; ?>
--- modules/custom/cu_alerts/js/cu_alerts.js
+++ modules/custom/cu_alerts/js/cu_alerts.js
@@ -5,11 +5,11 @@
       // Create our own timestamp
 			var stamp = Math.ceil( $.now() / 10000 );
 			//console.log(stamp);
-			
+
 			$.ajax({
 			  type: "GET",
 				// @TODO: get url from jQuery.extend(Drupal.settings
-				url: "http://www.getrave.com.global.prod.fastly.net/rss/cuboulder/channel1?stamp=" + stamp,
+				url: Drupal.settings.rave_alerts_url + "?stamp=" + stamp,
 				dataType: "xml",
 				//cache: false, // Causes Varnish to miss every request
 				success: rssParser,
@@ -17,20 +17,25 @@
 				  // Schedule the next request when the current one's complete
 					setTimeout(alertWorker, 10000);
 				}
-			});		
+			});
     });
-   
+
     function rssParser(xml) {
       $(xml).find("item").each(function () {
         if ($(this).find("link").text()) {
           feedLink = $(this).find("link").text();
         } else {
           // @TODO: get url from jQuery.extend(Drupal.settings
-          feedLink = 'http://alerts.colorado.edu';
+          feedLink = Drupal.settings.rave_alerts_site;
         }
-        
-        $("#cu-alerts .alert").html('<a href="' + feedLink + '" style="color:#fff;">' + $(this).find("title").text() + '</a> - ' + $(this).find("pubDate").text());
+        var stamp = Math.ceil( $.now() / 10000 );
+        var alertTitle = $(this).find("title").text();
+        var alertPubtime = $(this).find("pubDate").text();
+        $("#cu-alerts .alert").html(alertTitle + ' <a href="' + feedLink + '" >' + 'Read More</a>');
+        // data-alert-publish-time="' + alertPubtime + '" data-alert-timestamp="' + stamp + '"
+        $("#cu-alerts .alert").attr('data-alert-publish-time', alertPubtime);
+        $("#cu-alerts .alert").attr('data-alert-timestamp', stamp);
       });
-    }; 
-       
+    };
+
 })(jQuery);
