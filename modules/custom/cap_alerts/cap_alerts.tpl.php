0c5fe8a12e54706f9796b5ddc8d35326380cb30d
Kevin Reynen <kreynen@gmail.com>
2016-10-21 14:44:22 -0600

    altered RSS in cu_alters to use CAP in cap_alerts

--- modules/custom/cap_alerts/cap_alerts.tpl.php
+++ modules/custom/cap_alerts/cap_alerts.tpl.php
@@ -8,9 +8,9 @@
 <div id="cu-alerts" data-remote-url="<?php print $remote_url ?>">
   <?php if (variable_get('rave_alerts_enable', 1) && variable_get('rave_alerts_display', 1)): ?>
     <div class="alert">
-      <?php print $data['channel']['item']['description']; ?>
-      <?php if (!empty($data['channel']['item']['link'])) : ?>
-        <?php print l('Read More &raquo;', $data['channel']['item']['link'], array('html' => TRUE)); ?>
+      <?php print $data['info']['description']; ?>
+      <?php if (!empty($data['info']['instruction'])) : ?>
+        <?php print l('Read More &raquo;', $data['info']['instruction'], array('html' => TRUE)); ?>
       <?php elseif ($rave_alerts_site = variable_get('rave_alerts_enable', NULL)): ?>
         <?php print l('Read More &raquo;', $rave_alerts_site, array('html' => TRUE)); ?>
       <?php endif; ?>
--- modules/custom/cap_alerts/js/cap_alerts.js
+++ modules/custom/cap_alerts/js/cap_alerts.js
@@ -4,17 +4,17 @@
     function alertWorker() {
       // @TODO: Add loop count or option to exit so this doesn't keep running after an 
       // active event has ended if someone leaves a browser open
-      if ( Drupal.settings.rave_alerts_active_event == 1 ) {
+      if ( Drupal.settings.cap_alerts_active_event == 1 ) {
 				// Create our own rounded timestamp to limit paths cached by varnish
 				var stamp = Math.ceil( $.now() / 10000 );
 				//console.log(stamp);
 
 				$.ajax({
 					type: "GET",
-					url: Drupal.settings.rave_alerts_rss_url + "?stamp=" + stamp,
+					url: Drupal.settings.cap_alerts_url + "?stamp=" + stamp,
 					dataType: "xml",
 					//cache: false, // Causes Varnish to miss every request
-					success: rssParser,
+					success: capAlertParser,
 					complete: function() {
 						// Schedule the next request when the current one's complete
 						setTimeout(alertWorker, 10000);
@@ -23,21 +23,22 @@
 			};
     });
 
-    function rssParser(xml) {
-      $(xml).find("item").each(function () {
-        if ($(this).find("link").text()) {
-          feedLink = $(this).find("link").text();
+    function capAlertParser(xml) {
+      var alertPubtime = $(this).find("sent").text();
+      $(xml).find("info").each(function () {
+        if ($(this).find("instruction").text()) {
+          link = $(this).find("instruction").text();
         } else {
           // @TODO: get url from jQuery.extend(Drupal.settings
-          feedLink = Drupal.settings.rave_alerts_deafult_read_more_url;
+          link = Drupal.settings.cap_alerts_deafult_read_more_url;
         }
         var stamp = Math.ceil( $.now() / 10000 );
         var alertTitle = $(this).find("description").text();
-        var alertPubtime = $(this).find("pubDate").text();
-        $("#cu-alerts .alert").html(alertTitle + ' <a href="' + feedLink + '" >' + 'Read More</a>');
+        
+        $("#cap-alerts .alert").html(alertTitle + ' <a href="' + link + '" >' + 'Read More</a>');
         // data-alert-publish-time="' + alertPubtime + '" data-alert-timestamp="' + stamp + '"
-        $("#cu-alerts .alert").attr('data-alert-publish-time', alertPubtime);
-        $("#cu-alerts .alert").attr('data-alert-timestamp', stamp);
+        $("#cap-alerts .alert").attr('data-alert-publish-time', alertPubtime);
+        $("#cap-alerts .alert").attr('data-alert-timestamp', stamp);
       });
     };
 
