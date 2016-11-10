561085f272d5ed14097ad4fadd4e4ecc2990ee83
Owen Morrill <o.morrill@aggiemail.usu.edu>
2016-11-10 14:11:04 -0700

    FIT-2151 Add Who's online block to admin/dashboard/user.

--- modules/custom/express_dashboards/express_dashboard_user/templates/express-dashboard-user-profile.tpl.php
+++ modules/custom/express_dashboards/express_dashboard_user/templates/express-dashboard-user-profile.tpl.php
@@ -1,4 +1,10 @@
 <div class="express-dashboard-user-profile-wrapper">
+  <div class="col-md-2 pull-right">
+    <?php
+      $block = module_invoke('user', 'block_view', 'online');
+      print render($block['content']);
+    ?>
+  </div>
   <p>
     User since: <?php print date('F j, Y', $user['created']); ?>.
   </p>
