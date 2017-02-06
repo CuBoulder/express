1d633203823de013065d86f560c69a38bfdbc2c0
kevincrafts <kevin.crafts@colorado.edu>
2017-02-06 08:48:22 -0700

    adding match height option for block rows FIT-2424

---
+++ modules/custom/cu_block_row/js/cu-block-row.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(document).ready(function(){
+    $('.block-row-wrapper .block-row-match-height').each(function() {
+        $('.block .block-inner-wrapper', this).matchHeight(false);
+    });
+  });
+})(jQuery);
