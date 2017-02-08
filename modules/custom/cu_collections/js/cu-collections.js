49612ffcc30123e95ae2c25779597839ab9fa2cd
kevincrafts <kevin.crafts@colorado.edu>
2017-02-08 14:05:50 -0700

    improve collection performance FIT-2450

--- modules/custom/cu_collections/js/cu-collections.js
+++ modules/custom/cu_collections/js/cu-collections.js
@@ -7,14 +7,25 @@
 
     $("ul.collection-items-navigation a").first().addClass('active');
     $("ul.collection-items-navigation a").click(function(){
-      $(".collection-items").hide();
-      var target = $(this).attr("href");
-      $(target).fadeIn();
+      var collectionTarget = $(this).attr("data-collection");
+      $("#" + collectionTarget + " .collection-item").removeClass('collection-item-disabled');
+      var target = $(this).attr("data-collection-category");
+
+      $('#' + collectionTarget + ' .collection-item').not('.collection-category-' + target).addClass('collection-item-disabled');
+
       $("ul.collection-items-navigation a").removeClass('active');
       $(this).addClass('active');
       return false;
     });
+    $("ul.collection-items-navigation a.collection-category-all").click(function(){
+      var collectionTarget = $(this).attr("data-collection");
 
+      $("#" + collectionTarget + " .collection-item").removeClass('collection-item-disabled');
+
+      $("ul.collection-items-navigation a").removeClass('active');
+      $(this).addClass('active');
+      return false;
+    });
     $("select.collection-filter").change(function(){
       $(".collection-items").hide();
       var target = $(this).val();
--- modules/custom/cu_collections/templates/node--collection-item--grid.tpl.php
+++ modules/custom/cu_collections/templates/node--collection-item--grid.tpl.php
@@ -1,5 +1,5 @@
 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-<div class="collection-view-mode-grid">
+<div class="collection-item collection-view-mode-grid <?php print $category_classes; ?>">
   <?php
     if (!empty($content['field_collection_thumbnail'])) {
       print render($content['field_collection_thumbnail']);
