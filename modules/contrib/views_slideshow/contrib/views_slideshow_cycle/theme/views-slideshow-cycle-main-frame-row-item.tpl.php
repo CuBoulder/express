ff59e9a99f753fcb885a96757343ce7209d244f8
O Morrill <owenpm3@1644952.no-reply.drupal.org>
2017-02-21 09:32:24 -0700

    feature/FIT-2466 back to 3.1

--- modules/contrib/views_slideshow/contrib/views_slideshow_cycle/theme/views-slideshow-cycle-main-frame-row-item.tpl.php
+++ modules/contrib/views_slideshow/contrib/views_slideshow_cycle/theme/views-slideshow-cycle-main-frame-row-item.tpl.php
@@ -1,15 +1,3 @@
-<?php
-
-/**
- * @file
- * Views Slideshow cycle: Main frame row item.
- *
- * - $classes: Classes.
- * - $item: Row item.
- *
- * @ingroup views_templates
- */
-?>
 <div class="<?php print $classes; ?>">
   <?php print $item; ?>
 </div>
--- modules/contrib/views_slideshow/theme/views-slideshow-controls-text-previous.tpl.php
+++ modules/contrib/views_slideshow/theme/views-slideshow-controls-text-previous.tpl.php
@@ -1,16 +1,1 @@
-<?php
-
-/**
- * @file
- * Views Slideshow: Template for text control - previous.
- *
- * - $variables: Contains theme variables.
- * - $classes: Text control classes.
- * - $vss_id: The Views Slideshow unique ID.
- *
- * @ingroup views_templates
- */
-?>
-<span id="views_slideshow_controls_text_previous_<?php print $vss_id; ?>" class="<?php print $classes; ?>">
-  <a href="#" rel="prev"><?php print t('Previous'); ?></a>
-</span>
+<span id="views_slideshow_controls_text_previous_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><?php print t('Previous'); ?></a></span>
