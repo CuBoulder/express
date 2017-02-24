0220216df751a733258fd25a5e0d92a772cda6db
Alex Finnarn <alexfinnarn@gmail.com>
2017-02-24 16:28:26 -0700

    updating how placeholders are used in Twitter username

--- tests/behat/pull-requests.sh
+++ tests/behat/pull-requests.sh
@@ -3,15 +3,23 @@
 
 # Under Gitflow the branch will always be after the last slash.
 BRANCH=$(echo $TRAVIS_PULL_REQUEST_BRANCH | awk -F/ '{print $NF}')
+echo "$BRANCH"
 
 # Try to run tests with branch tag.
-RESULT=$(./bin/behat --config behat.yml --verbose --tags @$BRANCH)
-echo "$RESULT"
+BRANCH_RESULT=$(./bin/behat --config behat.yml --verbose --tags @$BRANCH)
+echo "$BRANCH_RESULT"
 
 # If no branch tests are found, run the whole suite.
-SCENARIO=${RESULT:0:12}
+SCENARIO=${BRANCH_RESULT:0:12}
 if [ "$SCENARIO" = "No scenarios" ]
-then
+  then
   printf "\n\nRunning whole test suite\n\n"
-  ./bin/behat --config behat.yml --verbose --tags '~@exclude_all_bundles'
+  FULL_RESULT=$(./bin/behat --config behat.yml --verbose --tags '~@exclude_all_bundles')
+  if [[ "$FULL_RESULT" == *"failed)"* ]]
+    then
+    exit 1
+  fi
+elif [[ "$BRANCH_RESULT" == *"failed)"* ]]
+  then
+  exit 1
 fi
