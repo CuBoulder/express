b21ed1097ddd7044b18b799f351bbe998e9f174a
Alex Finnarn <alexfinnarn@gmail.com>
2017-03-10 11:51:57 -0700

    adding cu_search_cse module

---
+++ modules/features/cu_search_cse/js/google_cse.js
@@ -0,0 +1,9 @@
+(function() {
+  var cx = '004087084483837606684:bu5knhuvyxi';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
