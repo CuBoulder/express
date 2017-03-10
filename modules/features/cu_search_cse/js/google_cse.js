a2635103006ea264ffb563a9630ecfa93cf7dd0a
Alex Finnarn <alexfinnarn@gmail.com>
2017-03-10 14:15:51 -0700

    exporting context for search feature

--- modules/features/cu_search_cse/js/google_cse.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var cx = '004087084483837606684:bu5knhuvyxi';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
