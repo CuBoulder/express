0c43a9af408e66186a71fbe9324e760870695cf8
kevincrafts <kevin.crafts@colorado.edu>
2017-03-16 08:25:45 -0600

    adding intro to feature layout, adding background color option for title #830

--- modules/custom/express_layout/templates/express-layout-title-image.tpl.php
+++ modules/custom/express_layout/templates/express-layout-title-image.tpl.php
@@ -2,7 +2,7 @@
 <div class="feature-layout-title-image">
 
   <div class="feature-layout-title-image-overlay">
-    <?php if ($breadcrumb): ?>
+    <?php if (isset($breadcrumb)): ?>
       <div class="title-image-breadcrumbs-wrapper">
         <div class="title-image-breadcrumbs element-max-width-padding"><?php print $breadcrumb; ?></div>
       </div>
@@ -10,7 +10,11 @@
     <div class="feature-layout-title-image-page-title element-max-width-padding">
       <h1><?php print $title; ?></h1>
       <div class="title-image-byline"></div>
-
+      <?php if (isset($subhead)): ?>
+        <div class="subhead">
+          <?php print $subhead; ?>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
