4d7d86dcfd1249a33af3ee17bde36d6903ad961b
kevincrafts <kevin.crafts@colorado.edu>
2017-03-23 15:33:28 -0600

    removing uneccessary js #823

--- modules/custom/cu_block_row/js/cu-block-row.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  $(document).ready(function(){
-    $('.block-row-wrapper .block-row-match-height').each(function() {
-        //$('.block .block-inner-wrapper', this).matchHeight(false);
-    });
-  });
-})(jQuery);
