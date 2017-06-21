29ab3cf8d342a0ea0a321ae547671bd0a83b180c
kevincrafts <kevin.crafts@colorado.edu>
2017-06-21 13:56:10 -0600

    updating admin block styles to wrap, better positioning of block title icon, adding icon to primary tab #897

--- modules/custom/express_block_designer/js/express-block-designer-form.js
+++ modules/custom/express_block_designer/js/express-block-designer-form.js
@@ -1,13 +1,16 @@
 (function ($) {
   $(document).ready(function(){
-    var search = '<label for"icon-search-field" class="element-invisible">Icon Search</label><div class="icon-search"><input type="search" name="icon-search-field" id="icon-search-field" class="icon-search-field form-text" size="40" placeholder="Search by Icon Name"/></div><div class="icon-search-results element-invisible"></div>';
+    var search = '<label for"icon-search-field" class="element-invisible">Icon Search</label><div class="icon-search"><input type="search" name="icon-search-field" id="icon-search-field" class="icon-search-field form-text" size="40" placeholder="Search by Icon Name"/></div><div class="icon-search-results element-invisible1"></div>';
     $('#edit-icon #edit-exbd-icon').before(search);
 
     $('.icon-search-field').on('input', function(event){
       var query = $('.icon-search-field').val().toLowerCase();
+      var count = 0;
 
       if (query == '') {
         $('#edit-exbd-icon .form-item-exbd-icon').removeClass('icon-search-selected').removeClass('icon-search-hidden');
+        count = $('.form-item-exbd-icon').length;
+        $('.icon-search-results').text('');
       }
       else {
         $('#edit-exbd-icon .form-item-exbd-icon').removeClass('icon-search-selected');
@@ -16,7 +19,7 @@
 
         $(".form-item-exbd-icon input[value*='" + query + "']").parent().addClass('icon-search-selected');
 
-        var count = $('.form-item-exbd-icon.icon-search-selected').length;
+        count = $('.form-item-exbd-icon.icon-search-selected').length;
         $('.icon-search-results').text(count + ' icons found.');
       }
 
