39ea8d065a8f6f2ee6a9aa7d19af18f5b2503436
kevincrafts <kevin.crafts@colorado.edu>
2017-06-28 13:46:55 -0600

    using bootstrap grid classes instead of custom classes #1090

--- modules/custom/express_settings/templates/cache--landing.tpl.php
+++ modules/custom/express_settings/templates/cache--landing.tpl.php
@@ -9,7 +9,7 @@ of the site for yourself, other editors, and your site's visitors.</p>
 <h2>Web Express Cache Types</h2>
 
 <div class="express-cards">
-  <div class="express-card" data-width="one-third">
+  <div class="express-card col-lg-4 col-md-4 col-sm-6 col-xs-12">
     <div class="content">
       <h3>Browser Cache</h3>
       <p>
@@ -18,7 +18,7 @@ of the site for yourself, other editors, and your site's visitors.</p>
 
     </div>
   </div>
-  <div class="express-card" data-width="one-third">
+  <div class="express-card col-lg-4 col-md-4 col-sm-6 col-xs-12">
     <div class="content">
       <h3>Intermediate Cache</h3>
       <p>
@@ -27,7 +27,7 @@ of the site for yourself, other editors, and your site's visitors.</p>
 
     </div>
   </div>
-  <div class="express-card"  data-width="one-third">
+  <div class="express-card col-lg-4 col-md-4 col-sm-6 col-xs-12">
     <div class="content">
       <h3>Web Express Cache</h3>
       <p>
@@ -39,7 +39,7 @@ of the site for yourself, other editors, and your site's visitors.</p>
 </div>
 
 <div class="express-cards">
-  <div class="express-card" data-width="full">
+  <div class="express-card col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <div class="content">
       <h2>Which cache should be cleared?</h2>
       <p>
