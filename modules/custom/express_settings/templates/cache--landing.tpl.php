c1d3c3cb07e588250ea5bc2fb8d60f1913ef41d4
kevincrafts <kevin.crafts@colorado.edu>
2017-06-28 13:33:00 -0600

    better layout for cache page #1090

---
+++ modules/custom/express_settings/templates/cache--landing.tpl.php
@@ -0,0 +1,67 @@
+<p>To ensure the Web Express platform delivers fast websites, we cache or store information
+(such as web pages, images, and files) to reduce bandwidth usage and increase speed.</p>
+
+<p>Our caches are designed to keep themselves 'current', and under normal conditions you won't
+need to interact with them, but occasionally there are problems that require you to
+clear one or more cache. Each time you clear a cache, you temporarily reduce the performance
+of the site for yourself, other editors, and your site's visitors.</p>
+
+<h2>Web Express Cache Types</h2>
+
+<div class="express-cards">
+  <div class="express-card" data-width="one-third">
+    <div class="content">
+      <h3>Browser Cache</h3>
+      <p>
+        Vivamus conubia Eleifend ridiculus pretium vivamus aenean. Et dolor rhoncus sapien curabitur a cum. Accumsan sem habitant. Tristique natoque tristique semper euismod porta mattis cum phasellus amet dapibus tincidunt nam, semper cum condimentum. Urna ipsum. Tortor. Consectetuer pharetra dis sociosqu.
+      </p>
+
+    </div>
+  </div>
+  <div class="express-card" data-width="one-third">
+    <div class="content">
+      <h3>Intermediate Cache</h3>
+      <p>
+        Vivamus conubia Eleifend ridiculus pretium vivamus aenean. Et dolor rhoncus sapien curabitur a cum. Accumsan sem habitant. Tristique natoque tristique semper euismod porta mattis cum phasellus amet dapibus tincidunt nam, semper cum condimentum. Urna ipsum. Tortor. Consectetuer pharetra dis sociosqu.
+      </p>
+
+    </div>
+  </div>
+  <div class="express-card"  data-width="one-third">
+    <div class="content">
+      <h3>Web Express Cache</h3>
+      <p>
+        Potenti duis. Ligula pellentesque. Dis erat vivamus habitasse conubia dictum proin cursus sollicitudin vehicula. Nullam velit enim inceptos morbi pharetra. Est urna faucibus ipsum, nisi inceptos curae; fermentum nunc facilisi conubia urna fermentum. Eros. Rutrum dictumst inceptos at non lorem.
+      </p>
+    </div>
+  </div>
+
+</div>
+
+<div class="express-cards">
+  <div class="express-card" data-width="full">
+    <div class="content">
+      <h2>Which cache should be cleared?</h2>
+      <p>
+        Vivamus conubia Eleifend ridiculus pretium vivamus aenean. Et dolor rhoncus sapien curabitur a cum. Accumsan sem habitant. Tristique natoque tristique semper euismod porta mattis cum phasellus amet dapibus tincidunt nam, semper cum condimentum. Urna ipsum. Tortor. Consectetuer pharetra dis sociosqu.
+      </p>
+      <div class="express-select-links">
+        <button>Select One</button>
+        <ul id="express-cache-select-options">
+          <li>
+            <a href="#">Choice Number One</a>
+          </li>
+          <li>
+            <a href="#">Choice Number Two</a>
+          </li>
+          <li>
+            <a href="#">Choice Number Three</a>
+          </li>
+          <li>
+            <a href="#">Choice Number Four</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
