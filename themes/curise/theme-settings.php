77a4b38a27073126aa8bdfc1fcd65d21033c94f8
kevincrafts <kevin.crafts@colorado.edu>
2017-08-01 15:08:59 -0600

    code formatting #896

--- themes/curise/theme-settings.php
+++ themes/curise/theme-settings.php
@@ -17,8 +17,8 @@ function curise_form_system_theme_settings_alter(&$form, &$form_state) {
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
-	$form['expressbase_theme_settings']['banner']['banner_color'] = array(
-	  '#type' => 'select',
+  $form['expressbase_theme_settings']['banner']['banner_color'] = array(
+    '#type' => 'select',
 	  '#title' => t('Banner Color'),
 	  '#default_value' => theme_get_setting('banner_color', 'curise') ? theme_get_setting('banner_color', 'curise') : 'black',
 	  '#description' => t('Pick a banner color for your site.'),
