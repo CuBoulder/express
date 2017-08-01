8e763e6133560aaadaadbcc12fee75430bb75282
kevincrafts <kevin.crafts@colorado.edu>
2017-08-01 15:09:57 -0600

    code formatting #896

--- themes/curise/theme-settings.php
+++ themes/curise/theme-settings.php
@@ -19,12 +19,12 @@ function curise_form_system_theme_settings_alter(&$form, &$form_state) {
   );
   $form['expressbase_theme_settings']['banner']['banner_color'] = array(
     '#type' => 'select',
-	  '#title' => t('Banner Color'),
-	  '#default_value' => theme_get_setting('banner_color', 'curise') ? theme_get_setting('banner_color', 'curise') : 'black',
-	  '#description' => t('Pick a banner color for your site.'),
-	  '#options' => array(
+    '#title' => t('Banner Color'),
+    '#default_value' => theme_get_setting('banner_color', 'curise') ? theme_get_setting('banner_color', 'curise') : 'black',
+    '#description' => t('Pick a banner color for your site.'),
+    '#options' => array(
       'dark' => t('Dark'),
       'light' => t('Light'),
     ),
-	);
+  );
 }
