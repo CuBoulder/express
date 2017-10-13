3c405e80c0d5a717a4de0ff5c0d2bf85cf060d23
kevincrafts <kevin.crafts@colorado.edu>
2017-10-13 16:53:36 -0600

    fixing issue where search path to colorado.edu was not set properly #1613

--- modules/features/cu_search/templates/google-appliance-block-form.tpl.php
+++ modules/features/cu_search/templates/google-appliance-block-form.tpl.php
@@ -30,7 +30,7 @@
    'value' => 'Colorado.edu',
    'placeholder' => 'Search Colorado.edu',
    'label' => 'Colorado.edu',
-   'action' => 'http://www.colorado.edu/gsearch',
+   'action' => 'http://www.colorado.edu/gsearch/all',
  );
 ?>
 <?php if (!empty($options)): ?>
