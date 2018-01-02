292fda2bd1cd794dcd6f2816ab36ffcabd908001
Alex Finnarn <alexfinnarn@gmail.com>
2018-01-02 10:17:57 -0700

    debug all environmental variables

--- test.php
+++ test.php
@@ -3,5 +3,17 @@
 if (isset($_SERVER['TRAVIS'])) {
   print 'on Travis.';
 } else {
-  print 'did not work.';
+  print 'not on Travis.';
+}
+
+if (isset($_SERVER['LANDO'])) {
+  print 'on Lando.';
+} else {
+  print 'not on Lando.';
+}
+
+if (defined('PANTHEON_ENVIRONMENT')) {
+  print 'on Pantheon.';
+} else {
+  print 'not on Pantheon.';
 }
