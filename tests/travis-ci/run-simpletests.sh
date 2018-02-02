1fa53bd23868b7aada7d7a86b4c99a3b0d2b5e07
Alex Finnarn <alexfinnarn@gmail.com>
2018-02-02 11:05:23 -0700

    run all tests

--- tests/travis-ci/run-simpletests.sh
+++ tests/travis-ci/run-simpletests.sh
@@ -5,7 +5,7 @@ cd ../../../../
 FOO=`pwd`
 echo ${FOO}
 
-OUTPUT=`php ./scripts/run-tests.sh --url http://127.0.0.1:8057 --php ~/.phpenv/versions/$(phpenv version-name)/bin/php`
+OUTPUT=`php ./scripts/run-tests.sh --url http://127.0.0.1:8057 --php ~/.phpenv/versions/$(phpenv version-name)/bin/php --all`
 echo ${OUTPUT}
 
 if [[ ${OUTPUT} == *fail,* ]]; then
