169bd235f3eeb0f4b95692c9ff773068cb98ec3b
kevincrafts <kevin.crafts@colorado.edu>
2018-02-07 08:48:42 -0700

    adding check for custom logo

--- themes/expressbase/templates/site-name.tpl.php
+++ themes/expressbase/templates/site-name.tpl.php
@@ -1,5 +1,7 @@
 <?php
-    // Load logo files
+  $custom_logo = array();
+  // Load logo files
+  if (variable_get('custom_white_logo', NULL) && variable_get('custom_black_logo', NULL)) {
     $custom_logo = array(
       'white' => file_load(variable_get('custom_white_logo')),
       'black' => file_load(variable_get('custom_black_logo')),
@@ -7,7 +9,8 @@
     // Create img markup
     $custom_logo['white']->markup = '<img class="custom-logo custom-logo-white" src="' . file_create_url($custom_logo['white']->uri) . '" alt="' . check_plain($site_name) . ' logo" />';
     $custom_logo['black']->markup = '<img class="custom-logo custom-logo-black" src="' . file_create_url($custom_logo['black']->uri) . '" alt="' . check_plain($site_name) . ' logo" />';
-  ?>
+  }
+?>
 <?php if (!empty($custom_logo)):?>
   <<?php print $site_name_tag; ?> class="site-name">
     <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="" rel="home"><span><?php print $custom_logo['black']->markup; ?></span></a>
