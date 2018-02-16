c46c813da95163fa2404a86bb517ad57dfb2640b
kevincrafts <kevin.crafts@colorado.edu>
2018-02-16 11:48:06 -0700

    changing #home-link to .home-link to fix tenon error #1938

--- themes/expressbase/template.php
+++ themes/expressbase/template.php
@@ -686,7 +686,8 @@ function expressbase_home_icon(&$vars, $menu) {
       if($vars[$menu][$key]['href'] == '<front>') {
         $vars[$menu][$key]['html'] = TRUE;
         $vars[$menu][$key]['title'] = '<i class="fa fa-home"></i><span class="element-invisible">Home</span>';
-        $vars[$menu][$key]['attributes']['id'] = 'home-link';
+        $vars[$menu][$key]['attributes']['class'][] = 'home-link';
+        dpm($vars[$menu][$key]['attributes']);
       }
     }
   }
