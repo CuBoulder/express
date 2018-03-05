594b597e34a3d716ac98ed3dab76282bbcc305b3
owenpm3 <o.morrill@aggiemail.usu.edu>
2018-03-05 13:33:58 -0700

    feature/1784 Update path to include cores/drupal.

--- libraries/simplesamlphp/config/config.php
+++ libraries/simplesamlphp/config/config.php
@@ -8,11 +8,12 @@ if (!ini_get('session.save_handler')) {
   ini_set('session.save_handler', 'file');
 }
 
-$drupal_dir = '../../../../../cores/drupal-current/'; // wherever Drupal is
+$drupal_dir = readlink('../../../../cores/drupal/drupal-current/'); // wherever Drupal is
 define('DRUPAL_ROOT', $drupal_dir);
 header('Access-Control-Allow-Origin: *');
 include_once(DRUPAL_ROOT . '/sites/default/settings.php');
 $db = $databases['saml']['default'];
+var_dump($drupal_dir);
 
 $url = $_SERVER['SERVER_NAME'];
 $path = $_SERVER['REQUEST_URI'];
