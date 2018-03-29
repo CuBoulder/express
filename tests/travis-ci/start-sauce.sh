34b47cf1a5c0bb6b245d0237f23dfcaf02a13735
Alex Finnarn <alexfinnarn@gmail.com>
2018-03-29 15:03:41 -0600

    try timeout for Sauce Connect

--- tests/travis-ci/start-sauce.sh
+++ tests/travis-ci/start-sauce.sh
@@ -8,8 +8,11 @@ if [ "${BEHAT_TAGS}" == "@javascript&&~exclude_all_bundles&&~broken" ]; then
   sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY > foo.txt > /dev/null 2>&1
 
   # While the process is starting, look for the line that tells you Sauce is up and then continue.
-  while [[ -z "$(tail -1 foo.txt | head | grep 'Sauce Connect is up, you may start your tests.')" ]]; do
+  COUNT=0
+  while [ -z "$(tail -1 foo.txt | head | grep 'Sauce Connect is up, you may start your tests.')" ] && [ "${COUNT}" -lt "10" ]; do
     echo "Retry hit Sauce Connect..."
+    COUNT=$((${COUNT} + 1))
+    echo "Count is ${COUNT}"
     sleep 5
   done
   echo "Sauce Connect is up..."
