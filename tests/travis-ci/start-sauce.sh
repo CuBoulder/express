319f98ef37f76f80833fa7f32bf946a1a3dd0d37
Alex Finnarn <alexfinnarn@gmail.com>
2018-03-29 11:13:45 -0600

    tail file to check when Sauce Connect is up and running

--- tests/travis-ci/start-sauce.sh
+++ tests/travis-ci/start-sauce.sh
@@ -2,6 +2,15 @@
 
 # If the Behat tags include JavaScript, then start Sauce Connect.
 if [ "${BEHAT_TAGS}" == "@javascript&&~exclude_all_bundles&&~broken" ]; then
-  sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY > /dev/null 2>&1 &
-  nc -zvv 127.0.0.1 4445; out=$?; while [[ $out -ne 0 ]]; do echo "Retry hit Selenium port 4445..."; nc -zvv localhost 4445; out=$?; sleep 5; done
+
+  # Connect to Sauce proxy and write output to file in the background.
+  touch foo.txt
+  sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY > foo.txt > /dev/null 2>&1
+
+  # While the process is starting, look for the line that tells you Sauce is up and then continue.
+  while [[ -z "$(tail -1 foo.txt | head | grep 'Sauce Connect is up, you may start your tests.')" ]]; do
+    echo "Retry hit Sauce Connect..."
+    sleep 5
+  done
+  echo "Sauce Connect is up..."
 fi
