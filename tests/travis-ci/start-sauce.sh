85474b29f0cca571e799abe8975ca9ce59fd1c12
Alex Finnarn <alexfinnarn@gmail.com>
2018-03-29 09:56:29 -0600

    start sauce in the background

--- tests/travis-ci/start-sauce.sh
+++ tests/travis-ci/start-sauce.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # If the Behat tags include JavaScript, then start Sauce Connect.
-if [ "${BEHAT_TAGS}" == "@javascript&&~exclude_all_bundles&&~broken" ]; then sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY ; fi
+if [ "${BEHAT_TAGS}" == "@javascript&&~exclude_all_bundles&&~broken" ]; then
+  sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY > /dev/null 2>&1 &
+  nc -zvv 127.0.0.1 4445; out=$?; while [[ $out -ne 0 ]]; do echo "Retry hit Selenium port 4445..."; nc -zvv localhost 4445; out=$?; sleep 5; done
+fi
