b662124c6c53895d8ae8a139cad5598c5a212e33
Alex Finnarn <alexfinnarn@gmail.com>
2018-04-13 11:47:56 -0600

    run JS and build ===build ===js

--- tests/travis-ci/run-js-tests.sh
+++ tests/travis-ci/run-js-tests.sh
@@ -4,7 +4,7 @@ EXPRESS_COMMIT_HAS_JS="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print li
 echo "commit has JS? - ${EXPRESS_COMMIT_HAS_JS}"
 
 EXPRESS_COMMIT_HAS_MERGE="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep 'Merge pull request')"
-echo "commit has JS? - ${EXPRESS_COMMIT_HAS_JS}"
+echo "commit merged to dev? - ${EXPRESS_COMMIT_HAS_JS}"
 
 # Run JS tests if merging PR into dev or has JS in it.
 if [ "${EXPRESS_COMMIT_HAS_MERGE}" ] || [ "${EXPRESS_COMMIT_HAS_JS}" ]; then
