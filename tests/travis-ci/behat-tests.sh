62ce0984d6df50bd8271edcd5e7d158d470234c7
Alex Finnarn <alexfinnarn@gmail.com>
2018-04-19 16:12:51 -0600

    add behat.bundle.yml file

--- tests/travis-ci/behat-tests.sh
+++ tests/travis-ci/behat-tests.sh
@@ -6,15 +6,26 @@ $HOME/.composer/vendor/bin/drush runserver 127.0.0.1:8057 > /dev/null 2>&1 &
 nc -zvv 127.0.0.1 8057; out=$?; while [[ $out -ne 0 ]]; do echo "Retry hit port 8057..."; nc -zvv localhost 8057; out=$?; sleep 5; done
 earlyexit
 
+cd $ROOT_DIR/drupal/profiles/express
+SKIP_EXPRESS_TESTS="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '!==express')"
+echo "Build Express? - ${SKIP_EXPRESS_TESTS}"
+
 # Setting Behat environment variables is now done in behat.travis.yml for simplicity.
 
 # Run headless Behat tests.
-${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.travis.yml --verbose --tags '~@exclude_all_bundles&&~@broken&&~@javascript'
-earlyexit
+if [ ! "${SKIP_EXPRESS_TESTS}" ]; then
+  ${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.travis.yml --verbose --tags '~@exclude_all_bundles&&~@broken&&~@javascript'
+  earlyexit
 
-# Run JS Behat tests if merged into dev.
-${ROOT_DIR}/drupal/profiles/express/tests/travis-ci/run-js-tests.sh
-earlyexit
+  # Run JS Behat tests if merged into dev.
+  ${ROOT_DIR}/drupal/profiles/express/tests/travis-ci/run-js-tests.sh
+  earlyexit
+fi
+
+# Run bundle tests.
+if [ BUNDLE_NAME != "null" ]; then
+  ${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.bundle.yml --verbose --tags '~@exclude_all_bundles&&~@broken'
+fi
 
 # Output performance logging data.
 $HOME/.composer/vendor/bin/drush scr ${ROOT_DIR}/drupal/profiles/express/tests/travis-ci/log-express-performance.php
