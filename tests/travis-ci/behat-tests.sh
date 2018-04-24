362898bb09fc00732cb2a41a70dffc435e54fba7
Cathy Snider <catherine.snider@colorado.edu>
2018-04-24 10:48:27 -0600

    Update behat-tests.sh

--- tests/travis-ci/behat-tests.sh
+++ tests/travis-ci/behat-tests.sh
@@ -9,7 +9,7 @@ earlyexit
 # Setting Behat environment variables is now done in behat.travis.yml for simplicity.
 
 # Run headless Behat tests.
-${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.travis.yml --verbose --tags '@people&&@rebuild'
+${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.travis.yml --verbose --tags '@people'
 earlyexit
 
 # Run JS Behat tests if merged into dev.
