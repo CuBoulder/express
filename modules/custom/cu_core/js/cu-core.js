f7c4bdd9951f3c549c27dcbd401881d5c5cc8946
Kevin Reynen <kreynen@gmail.com>
2018-05-07 16:52:17 -0600

    added more logic to environment detection

--- modules/custom/cu_core/js/cu-core.js
+++ modules/custom/cu_core/js/cu-core.js
@@ -3,16 +3,16 @@
     $('#navbar-bar').prepend('<div class="express-environment-indicator"><span class="express-environment-indicator-label"></span> <a href="' + Drupal.settings.basePath + 'admin/settings/site-status/verify">Migration Status</a></div>');
     
     // Prod New Launched
-    $('.environment-production.infrastructure-new.status-launched .express-environment-indicator span').text('Live Site  - New Server Environment');
+    $('.environment-production.infrastructure-new.status-launched .express-environment-indicator .express-environment-indicator-label').text('Live Site  - New Server Environment');
     // Prod New Staged
-    $('.environment-production.infrastructure-new.status-staged span').text('Staged Site  - New Server Environment');
+    $('.environment-production.infrastructure-new.status-staged .express-environment-indicator-label').text('Staged Site  - New Server Environment');
     // Prod Old Launched
-    $('.environment-production.infrastructure-old.status-launched span').text('Live Site  - Old Server Environment');
+    $('.environment-production.infrastructure-old.status-launched .express-environment-indicator-label').text('Live Site  - Old Server Environment');
     // Prod Old Launched
-    $('.environment-production.infrastructure-old.status-staged span').text('Temporary Staging Site  - Old Server Environment');
+    $('.environment-production.infrastructure-old.status-staged .express-environment-indicator-label').text('Temporary Staging Site  - Old Server Environment');
 
     // Test Launched
-    $('.environment-test.status-launched span').text('Testing Site');
-    $('.environment-test.status-staged span').text('Temporary Training Site');
+    $('.environment-test.status-launched .express-environment-indicator-label').text('Testing Site');
+    $('.environment-test.status-staged .express-environment-indicator-label').text('Temporary Training Site');
   });
 }(jQuery));
