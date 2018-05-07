f381554d373e78c8f4b60ef658dc9ac5b5d9b844
kevincrafts <kevin.crafts@colorado.edu>
2018-05-07 12:27:47 -0600

    Adding js to inject environment indicator elements #2242

---
+++ modules/custom/cu_core/js/cu-core.js
@@ -0,0 +1,5 @@
+(function ($) {
+  $(document).ready(function(){
+     $('#navbar-bar').prepend('<div class="express-environment-indicator"><span class="express-environment-indicator-label"></span> <a href="#">Update Site Status</a></div>');
+  });
+}(jQuery));
