ab7eed56a1d6af3ac7e9346463236999badfac02
Kevin Reynen <kreynen@gmail.com>
2018-06-06 14:14:13 -0600

    moving embed template into profile

---
+++ modules/contrib/embed_templates/modules/et_tracking_pixels/templates/embed-templates-doubleclick.tpl.php
@@ -0,0 +1,17 @@
+<!--
+Start of DoubleClick Floodlight Tag: Please do not remove
+Activity name of this tag: <?php print check_plain($variables['activity']); ?>
+URL of the webpage where the tag is expected to be placed: <?php print check_plain($variables['url']); ?>
+This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+Creation Date: <?php print check_plain($variables['creation']) ?>
+-->
+<script type="text/javascript">
+  var axel = Math.random() + "";
+  var a = axel * 10000000000000;
+  document.write('<iframe src="https://<?php print check_plain($variables['account_id']); ?>.fls.doubleclick.net/activityi;src=<?php print check_plain($variables['account_id']); ?>;type=invmedia;cat=buyulyok;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+</script>
+<noscript>
+  <iframe src="https://<?php print check_plain($variables['account_id']); ?>.fls.doubleclick.net/activityi;src=<?php print check_plain($variables['account_id']); ?>;type=invmedia;cat=buyulyok;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+</noscript>
+<!-- End of DoubleClick Floodlight Tag: Please do not remove -->
+
