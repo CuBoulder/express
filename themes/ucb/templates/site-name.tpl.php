2c5a9b6d6ec4d5663fb1bfe34f1c3fb203c284f7
Kevin Crafts <kevin.crafts@colorado.edu>
2018-09-20 13:02:29 -0600

    fixing affiliation printing

--- themes/ucb/templates/site-name.tpl.php
+++ themes/ucb/templates/site-name.tpl.php
@@ -22,29 +22,24 @@
     <<?php print $site_name_tag; ?> class="site-name">
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
     </<?php print $site_name_tag; ?>>
-    <?php
-      $affiliation = variable_get('express_site_affiliation', FALSE);
-      $cu_affiliation = variable_get('cu_site_affiliation_options', FALSE);
-    ?>
-
-    <?php if ($affiliation || $cu_affiliation): ?>
-
-      <div class="affiliation">
-        <?php
-          if (!empty($affiliation) && $cu_affiliation == 'custom') {
-            if (!empty($affiliation['url'])) {
-              print l($affiliation['title'], $affiliation['url'], array('html' => TRUE));
-            }
-            else {
-              print $affiliation['title'];
-            }
-          }
-          elseif ($cu_affiliation != 'custom') {
-            $affiliation = cu_core_site_affiliation_options($cu_affiliation);
-            print l($affiliation['label'], $affiliation['url'], array('html' => TRUE));
-          }
-        ?>
-      </div>
-    <?php endif; ?>
   <?php endif; ?>
+<?php endif; ?>
+<?php
+  $affiliation = variable_get('express_site_affiliation', FALSE);
+  $cu_affiliation = variable_get('cu_site_affiliation_options', FALSE);
+?>
+
+<?php if ($affiliation || $cu_affiliation): ?>
+
+  <div class="affiliation">
+    <?php
+      if (!empty($affiliation) && $cu_affiliation == 'custom') {
+        print $affiliation;
+      }
+      elseif (!empty($cu_affiliation) && $cu_affiliation != 'custom') {
+        $affiliation = cu_core_site_affiliation_options($cu_affiliation);
+        print l($affiliation['label'], $affiliation['url'], array('html' => TRUE));
+      }
+    ?>
+  </div>
 <?php endif; ?>
