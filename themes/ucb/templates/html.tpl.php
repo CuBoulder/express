f6345f7257f45b132fb36ff0a89c13cd7ecdee65
Kevin Crafts <kevin.crafts@colorado.edu>
2018-12-06 09:10:54 -0700

    fixing broken css

--- themes/ucb/templates/html.tpl.php
+++ themes/ucb/templates/html.tpl.php
@@ -32,7 +32,7 @@
     <![endif]-->
   <?php endif; ?>
 </head>
-<body class="background-color-black <?php print $classes; ?>" <?php print $attributes;?>>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php if ($skip_link_text && $skip_link_anchor): ?>
     <p id="skip-link">
       <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
