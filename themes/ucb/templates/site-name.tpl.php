30509b53415746f52777acdbf5e70bb45e2eb912
Kevin Crafts <kevin.crafts@colorado.edu>
2018-12-18 11:03:59 -0700

    changes for new custom logos

--- themes/ucb/templates/site-name.tpl.php
+++ themes/ucb/templates/site-name.tpl.php
@@ -1,14 +1,14 @@
 <?php
   $custom_logo = array();
   // Load logo files
-  if (variable_get('custom_white_logo', NULL) && variable_get('custom_black_logo', NULL)) {
+  if (variable_get('cu_custom_white_logo', NULL) && variable_get('cu_custom_black_logo', NULL)) {
     $custom_logo = array(
-      'white' => file_load(variable_get('custom_white_logo')),
-      'black' => file_load(variable_get('custom_black_logo')),
+      'white' => file_load(variable_get('cu_custom_white_logo')),
+      'black' => file_load(variable_get('cu_custom_black_logo')),
     );
     // Create img markup
-    $custom_logo['white']->markup = '<img class="custom-logo custom-logo-white" src="' . file_create_url($custom_logo['white']->uri) . '" alt="' . check_plain($site_name) . ' logo" />';
-    $custom_logo['black']->markup = '<img class="custom-logo custom-logo-black" src="' . file_create_url($custom_logo['black']->uri) . '" alt="' . check_plain($site_name) . ' logo" />';
+    $custom_logo['white']->markup = '<img class="cu-custom-logo custom-logo-white" src="' . file_create_url($custom_logo['white']->uri) . '" alt="' . check_plain($site_name) . ' logo" />';
+    $custom_logo['black']->markup = '<img class="cu-custom-logo custom-logo-black" src="' . file_create_url($custom_logo['black']->uri) . '" alt="' . check_plain($site_name) . ' logo" />';
   }
 ?>
 <?php if (!empty($custom_logo)):?>
