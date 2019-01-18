1fbcc4c862064e0a69fcb15064aaf25b5080ace4
Alex Finnarn <alexfinnarn@gmail.com>
2019-01-18 10:04:46 -0500

    Issue #3020: Reorder bundle tests to go first (#3034)

--- tests/travis-ci/behat-tests.sh
+++ tests/travis-ci/behat-tests.sh
@@ -12,24 +12,8 @@ else
   cd ${ROOT_DIR}/drupal/profiles/express
 fi
 
-SKIP_EXPRESS_TESTS="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '!==express')"
-
-# Setting Behat environment variables is now done in behat.travis.yml for simplicity.
-
-# Run Behat Express tests when in a bundle repo if commit flag is set.
-if [ ! "${SKIP_EXPRESS_TESTS}" ]; then
-
-  echo "Running Express headless tests..."
-  ${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.travis.yml --verbose --tags ${EXPRESS_HEADLESS_BEHAT_TAGS}
-  earlyexit
-
-  # Run JS Behat tests if merged into dev.
-  ${ROOT_DIR}/drupal/profiles/express/tests/travis-ci/run-js-tests.sh
-  earlyexit
-
-fi
-
-# Run the bundle tests.
+# Run the bundle tests first to get more immediate feedback on failures.
+# Express tests take longer and bundle tests are more important to run during development.
 if [ "${BUNDLE_NAME}" != "null" ]; then
 
   # Enable bundle.
@@ -50,6 +34,22 @@ if [ "${BUNDLE_NAME}" != "null" ]; then
   echo "Running ${BUNDLE_NAME} bundle tests..."
   ${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.bundle.yml --verbose --tags ${BUNDLE_BEHAT_TAGS}
   earlyexit
+fi
+
+# Setting Behat environment variables is now done in behat.travis.yml for simplicity.
+
+# Run Behat Express tests when in a bundle repo if commit flag is set.
+SKIP_EXPRESS_TESTS="$(git log -2 --pretty=%B | awk '/./{line=$0} END{print line}' | grep '!==express')"
+if [ ! "${SKIP_EXPRESS_TESTS}" ]; then
+
+  echo "Running Express headless tests..."
+  ${ROOT_DIR}/drupal/profiles/express/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/drupal/profiles/express/tests/behat/behat.travis.yml --verbose --tags ${EXPRESS_HEADLESS_BEHAT_TAGS}
+  earlyexit
+
+  # Run JS Behat tests if merged into dev.
+  ${ROOT_DIR}/drupal/profiles/express/tests/travis-ci/run-js-tests.sh
+  earlyexit
+
 fi
 
 # Output performance logging data.
