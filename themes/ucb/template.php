07e480436344081c258803881a09bfac8720af84
Kevin Crafts <kevin.crafts@colorado.edu>
2019-01-31 12:42:15 -0700

    moving action menu to after main menu for better accessibility

--- themes/ucb/template.php
+++ themes/ucb/template.php
@@ -751,6 +751,7 @@ function ucb_home_icon(&$vars, $menu) {
         $vars[$menu][$key]['html'] = TRUE;
         $vars[$menu][$key]['title'] = '<i class="fa fa-home"></i><span class="element-invisible">Home</span>';
         $vars[$menu][$key]['attributes']['id'] = 'home-link';
+        $vars[$menu][$key]['attributes']['class'][] = 'home-link';
       }
     }
   }
