e8bdbf30a0e1ac4d449caff8c56226075f55e600
Kevin Reynen <kreynen@gmail.com>
2019-02-05 15:30:51 -0700

    updated core testing to 7.63

--- tests/travis-ci/install.sh
+++ tests/travis-ci/install.sh
@@ -11,8 +11,8 @@ earlyexit
 
 # Build Codebase.
 cd $ROOT_DIR
-drush dl drupal-7.62
-mkdir drupal && mv drupal-7.62/* drupal/
+drush dl drupal-7.63
+mkdir drupal && mv drupal-7.63/* drupal/
 mkdir profiles && mv express drupal/profiles/
 
 # Harden Codebase.
