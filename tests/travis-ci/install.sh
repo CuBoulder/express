fb3ab43fecc77059680007ccfe425ae7f72e2331
Kevin Reynen <kreynen@gmail.com>
2019-03-20 11:33:28 -0600

    updated tests to use 7.65

--- tests/travis-ci/install.sh
+++ tests/travis-ci/install.sh
@@ -11,8 +11,8 @@ earlyexit
 
 # Build Codebase.
 cd $ROOT_DIR
-drush dl drupal-7.64
-mkdir drupal && mv drupal-7.64/* drupal/
+drush dl drupal-7.65
+mkdir drupal && mv drupal-7.65/* drupal/
 mkdir profiles && mv express drupal/profiles/
 
 # Harden Codebase.
