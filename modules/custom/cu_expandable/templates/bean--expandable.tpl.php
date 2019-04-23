2d1902b29e46d91a3356af88cf54f93371cc2935
Kevin Crafts <kevin.crafts@colorado.edu>
2019-04-23 13:21:57 -0600

    fixing issue where expandable block didn't have good contrast when used with different backgrounds #3252

--- modules/custom/cu_expandable/templates/bean--expandable.tpl.php
+++ modules/custom/cu_expandable/templates/bean--expandable.tpl.php
@@ -13,16 +13,17 @@
           $label = trim(render($label));
         ?>
         <li class="expandable-tablist-item " role="presentation">
-          <a href="#<?php print cu_expandable_system_label($label); ?>-<?php print $variables['bean']->bid; ?>" id="label_<?php print cu_expandable_system_label($label); ?>-<?php print $variables['bean']->bid; ?>" class="expandable-tablist-link" role="tab" data-expandable-panel="<?php print cu_expandable_system_label($label); ?>-<?php print $variables['bean']->bid; ?>" data-tabset="expandable-<?php print $variables['bean']->bid; ?>" tabindex="0"><?php print render($label); ?></a>
+          <a href="#<?php print cu_expandable_system_label($label); ?>-<?php print $variables['bean']->bid; ?>" id="label_<?php print cu_expandable_system_label($label); ?>-<?php print $variables['bean']->bid; ?>" class="expandable-tablist-link text-color" role="tab" data-expandable-panel="<?php print cu_expandable_system_label($label); ?>-<?php print $variables['bean']->bid; ?>" data-tabset="expandable-<?php print $variables['bean']->bid; ?>" tabindex="0"><?php print render($label); ?></a>
         </li>
       <?php endforeach; ?>
 
     </ul>
   </div>
-  <div class="expandable-panel-group">
+  <div class="expandable-panel-group ucb-colors">
 
   <?php foreach ($content['sections'] as $section):?>
     <?php $section['bid'] = $variables['bean']->bid; ?>
+    <?php $section['display'] = $content['display']; ?>
     <?php print theme('expandable_section_tabs', array('expandable' => $section)); ?>
   <?php endforeach; ?>
   </div>
